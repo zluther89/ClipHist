920d90eecb9ce4810c61869d72337a1c70996be9
Zachary Luther <Zluther89@gmail.com>
2020-08-07 16:32:45 -0400

    write command

--- ReadClip/ReadClip.go
+++ ReadClip/ReadClip.go
@@ -10,6 +10,11 @@ func getReadCmd() *exec.Cmd {
 	return exec.Command("pbpaste")
 }
 
+//returns command to copy to clipboard
+func getCopyCmd() *exec.Cmd {
+	return exec.Command("pbcopy")
+}
+
 // returns string results of paste command
 func ReadClip() string {
 	pasteCmd := getReadCmd()
@@ -18,4 +23,26 @@ func ReadClip() string {
 		fmt.Println(err)
 	}
 	return string(res)
+}
+
+//takes in a string, writes string to clipboard, returns err
+func WriteToClip(s string) error {
+	writeCmd := getCopyCmd()
+	writer, err := writeCmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	if err := writeCmd.Start(); err != nil {
+		return err
+	}
+
+	if _, err := writer.Write([]byte(s)); err != nil {
+		return err
+	}
+
+	if err := writer.Close(); err != nil {
+		return err
+	}
+	return writeCmd.Wait()
 }

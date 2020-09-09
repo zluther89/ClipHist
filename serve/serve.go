0f2bc35dfa4f76c1bc628ebb98678d84e46c9965
Zachary Luther <Zluther89@gmail.com>
2020-09-08 22:58:41 -0400

    websocket init

--- serve/serve.go
+++ serve/serve.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"ClipHist/Clip"
 	"ClipHist/ClipDB"
 	"ClipHist/Handlers"
+	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/websocket"
 )
 
 var port string = ":3000"
 
 func main() {
-
+	notify := make(chan bool)
 	db, err := ClipDB.Init("../sqliteDb/ClipHist.db")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 	h := Handlers.Handler{DB: db}
 
+	go Clip.ChanStart(notify, db)
+	go h.Listen(notify)
+
 	http.Handle("/", http.FileServer(http.Dir("../ClipHistyFE/public")))
 	http.HandleFunc("/content", h.ContentHandler)
+	http.Handle("/socket", websocket.Handler(h.Socket))
 
 	log.Print("Listening on port", port)
 	log.Fatal(http.ListenAndServe(port, nil))

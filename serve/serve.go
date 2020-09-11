eae5e5597c3c8829854546a160cfa4b3ad4e280d
Zachary Luther <Zluther89@gmail.com>
2020-09-10 22:09:07 -0400

    creating better notification chnl

--- serve/serve.go
+++ serve/serve.go
@@ -13,20 +13,44 @@ import (
 
 var port string = ":3000"
 
+type message struct {
+	Message string `json:"message"`
+}
+
 func main() {
-	notify := make(chan bool)
+	//notify := make(chan bool)
 	db, err := ClipDB.Init("../sqliteDb/ClipHist.db")
 	if err != nil {
 		fmt.Println(err)
 	}
 	h := Handlers.Handler{DB: db}
 
-	go Clip.ChanStart(notify, db)
 	//go h.Listen(notify)
+	connCount := 0
 
 	http.Handle("/", http.FileServer(http.Dir("../ClipHistyFE/public")))
 	http.HandleFunc("/content", h.ContentHandler)
-	http.Handle("/socket", websocket.Handler(Handlers.Wrapper(notify)))
+	http.Handle("/socket", websocket.Handler(func(ws *websocket.Conn) {
+		if connCount >= 1 {
+			ws.Close()
+			return
+		}
+
+		connCount += 1
+		var m message
+
+		for {
+			go Clip.ChanStart(db, ws, "A CHANGE")
+			// receive a message using the codec
+			if err := websocket.JSON.Receive(ws, &m); err != nil {
+				log.Println("websocket recieve:", err)
+				connCount -= 1
+				return
+			}
+			log.Println("Received message:", m.Message)
+		}
+
+	}))
 
 	log.Print("Listening on port", port)
 	log.Fatal(http.ListenAndServe(port, nil))
